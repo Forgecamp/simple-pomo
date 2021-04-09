3f7094f62075452b33a6491066baac365b08ea0e
D. Williams <thedavesignal@gmail.com>
2021-04-09 16:26:09 -0700

    wip boilerplates for screens

--- screens/TimerScreen/index.js
+++ screens/TimerScreen/index.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 const TimerScreen = () => {
     return (
         <View>
-            <Text>Test.</Text>
+            <Text>Main Timer Screen</Text>
         </View>
     );
 };
---
+++ screens/UserPreferencesScreen/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const UserPreferencesScreen = () => {
+    return (
+        <View>
+            <Text>User Preferences Screen</Text>
+        </View>
+    );
+};
+
+export default UserPreferencesScreen;
