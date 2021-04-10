ba727669387983cbb455735428eaa54e7b5f88b6
D. Williams <thedavesignal@gmail.com>
2021-04-09 17:07:57 -0700

    wip initial wireup of stack nav to a drawer nav

--- screens/TimerScreen/index.js
+++ screens/TimerScreen/index.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const TimerScreen = () => {
     return (
-        <View>
+        <View style={styles.main}>
             <Text>Main Timer Screen</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+    },
+});
 
 export default TimerScreen;
--- shared/navigation/AppNavigator/index.js
+++ shared/navigation/AppNavigator/index.js
@@ -2,19 +2,66 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import TimerScreen from "../../../screens/TimerScreen";
+import AboutScreen from "../../../screens/AboutScreen";
+import UserPreferencesScreen from "../../../screens/UserPreferencesScreen";
 
-const AppStackNavigator = createStackNavigator();
+const TimerStackNavigator = createStackNavigator();
+const AboutStackNavigator = createStackNavigator();
+const UserPrefsStackNavigator = createStackNavigator();
+const AppDrawerNavigator = createDrawerNavigator();
+
+const TimerNavigator = () => {
+    return (
+        <TimerStackNavigator.Navigator>
+            <TimerStackNavigator.Screen
+                component={TimerScreen}
+                name="Timer"
+                options={{
+                    title: "SimplePomo",
+                }}
+            />
+        </TimerStackNavigator.Navigator>
+    );
+};
+
+const AboutNavigator = () => {
+    return (
+        <AboutStackNavigator.Navigator>
+            <AboutStackNavigator.Screen component={AboutScreen} name="About" />
+        </AboutStackNavigator.Navigator>
+    );
+};
+
+const UserPrefsNavigator = () => {
+    return (
+        <UserPrefsStackNavigator.Navigator>
+            <UserPrefsStackNavigator.Screen
+                component={UserPreferencesScreen}
+                name="Preferences"
+            />
+        </UserPrefsStackNavigator.Navigator>
+    );
+};
 
 export const AppNavigator = () => {
     return (
         <NavigationContainer>
-            <AppStackNavigator.Navigator>
-                <AppStackNavigator.Screen
-                    component={TimerScreen}
-                    name="TimerScreen"
+            <AppDrawerNavigator.Navigator>
+                <AppDrawerNavigator.Screen
+                    component={TimerNavigator}
+                    name="TimerNavigator"
                 />
-            </AppStackNavigator.Navigator>
+                <AppDrawerNavigator.Screen
+                    component={AboutNavigator}
+                    name="AboutNavigator"
+                />
+                <AppDrawerNavigator.Screen
+                    component={UserPrefsNavigator}
+                    name="UserPrefsNavigator"
+                />
+            </AppDrawerNavigator.Navigator>
         </NavigationContainer>
     );
 };
