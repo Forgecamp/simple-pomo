52b3dc0c16c2b45d0dae869445585859e73c6300
David Williams <thedavesignal@gmail.com>
2021-04-12 14:44:38 -0700

    wip ensuring countdown circle package is suitable

--- screens/TimerScreen/index.js
+++ screens/TimerScreen/index.js
@@ -1,10 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Animated } from "react-native";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+import ExpoConstants from "expo-constants";
 
 const TimerScreen = () => {
     return (
         <View style={styles.main}>
-            <Text>Main Timer Screen</Text>
+            <View style={styles.clock}>
+                {/* <Text>Yo</Text> */}
+                <CountdownCircleTimer
+                    isPlaying
+                    duration={10}
+                    initialRemainingTime={9}
+                    colors={[
+                        ["#004777", 0.4],
+                        ["#F7B801", 0.4],
+                        ["#A30000", 0.2],
+                    ]}
+                >
+                    {({ remainingTime, animatedColor }) => {
+                        if (remainingTime === 0) console.log("yo");
+                        return (
+                            <Animated.Text style={{ color: animatedColor }}>
+                                {remainingTime}
+                            </Animated.Text>
+                        );
+                    }}
+                </CountdownCircleTimer>
+            </View>
         </View>
     );
 };
@@ -12,6 +35,11 @@ const TimerScreen = () => {
 const styles = StyleSheet.create({
     main: {
         flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingBottom: ExpoConstants.statusBarHeight,
+        backgroundColor: "#ecf0f1",
+        padding: 8,
     },
 });
 
