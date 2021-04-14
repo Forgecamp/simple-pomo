ac7d134f50af69b9f0d9e748e301a6f4339df3bc
D. Williams <thedavesignal@gmail.com>
2021-04-14 15:44:56 -0700

    wip some basic icons

--- screens/TimerScreen/index.js
+++ screens/TimerScreen/index.js
@@ -1,6 +1,11 @@
+// Core/First Party
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Animated, Button } from "react-native";
+import { View, StyleSheet, Animated, Platform, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+// Additional Modules/Components
+// Third Party Packages
 import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+// Constants
 import ExpoConstants from "expo-constants";
 import * as ColorConstants from "../../shared/constants/Colors";
 
@@ -12,32 +17,60 @@ const TimerScreen = () => {
 
     return (
         <View style={styles.main}>
-            <View style={styles.clock}>
+            <View style={styles.timerFace}>
                 <CountdownCircleTimer
                     key={key}
                     isPlaying={isRunning}
                     duration={startingDuration}
                     initialRemainingTime={startingTime}
-                    colors={[[ColorConstants.Success, 1.0]]}
+                    colors={[[ColorConstants.Notice, 1.0]]}
                     size={250}
                 >
                     {({ remainingTime, animatedColor }) => {
-                        let minutes = Math.floor(remainingTime / 60).toString();
+                        let minutes = Math.floor(remainingTime / 60)
+                            .toString()
+                            .padStart(2, "0");
                         let seconds = (remainingTime % 60)
                             .toString()
                             .padStart(2, "0");
                         return (
-                            <Animated.Text
-                                style={{
-                                    color: animatedColor,
-                                    ...styles.interiorText,
-                                }}
-                            >
-                                {minutes} : {seconds}
-                            </Animated.Text>
+                            <View style={styles.timerInterior}>
+                                <Ionicons
+                                    name="md-reload"
+                                    size={24}
+                                    color={ColorConstants.Notice}
+                                    style={{ display: "none" }}
+                                />
+                                <Animated.Text
+                                    style={{
+                                        color: animatedColor,
+                                        ...styles.interiorText,
+                                    }}
+                                >
+                                    {minutes} : {seconds}
+                                </Animated.Text>
+                                <Ionicons
+                                    name="md-reload"
+                                    size={24}
+                                    color={ColorConstants.Notice}
+                                    style={{ paddingTop: 15 }}
+                                />
+                            </View>
                         );
                     }}
                 </CountdownCircleTimer>
+            </View>
+            <View style={styles.controlBar}>
+                <Ionicons
+                    name="md-play"
+                    size={48}
+                    color={ColorConstants.Notice}
+                />
+                <Ionicons
+                    name="md-stop"
+                    size={48}
+                    color={ColorConstants.Notice}
+                />
             </View>
         </View>
     );
@@ -46,14 +79,27 @@ const TimerScreen = () => {
 const styles = StyleSheet.create({
     main: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
-        paddingBottom: ExpoConstants.statusBarHeight + 20,
+        marginTop: 2.5 * ExpoConstants.statusBarHeight,
         backgroundColor: "#ecf0f1",
-        // padding: 8,
     },
     interiorText: {
         fontSize: 24,
+    },
+    timerInterior: {
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        flexDirection: "column",
+        width: "100%",
+        paddingTop: 42,
+    },
+    controlBar: {
+        flexDirection: "row",
+        paddingTop: 25,
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        width: 200,
     },
 });
 
