5a7f6f0b5360ad20161e69772119e10627367fe1
D. Williams <thedavesignal@gmail.com>
2021-04-14 12:01:19 -0700

    wip positioning timer and colors

--- screens/TimerScreen/index.js
+++ screens/TimerScreen/index.js
@@ -1,27 +1,39 @@
-import React from "react";
-import { View, Text, StyleSheet, Animated } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Animated, Button } from "react-native";
 import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
 import ExpoConstants from "expo-constants";
+import * as ColorConstants from "../../shared/constants/Colors";
 
 const TimerScreen = () => {
+    const [startingTime, setStartingTime] = useState(900);
+    const [startingDuration, setStartingDuration] = useState(900);
+    const [key, setKey] = useState(0);
+    const [isRunning, setIsRunning] = useState(true);
+
     return (
         <View style={styles.main}>
             <View style={styles.clock}>
                 <CountdownCircleTimer
-                    isPlaying
-                    duration={10}
-                    initialRemainingTime={9}
-                    colors={[
-                        ["#004777", 0.4],
-                        ["#F7B801", 0.4],
-                        ["#A30000", 0.2],
-                    ]}
+                    key={key}
+                    isPlaying={isRunning}
+                    duration={startingDuration}
+                    initialRemainingTime={startingTime}
+                    colors={[[ColorConstants.Success, 1.0]]}
+                    size={250}
                 >
                     {({ remainingTime, animatedColor }) => {
-                        if (remainingTime === 0) console.log("yo");
+                        let minutes = Math.floor(remainingTime / 60).toString();
+                        let seconds = (remainingTime % 60)
+                            .toString()
+                            .padStart(2, "0");
                         return (
-                            <Animated.Text style={{ color: animatedColor }}>
-                                {remainingTime}
+                            <Animated.Text
+                                style={{
+                                    color: animatedColor,
+                                    ...styles.interiorText,
+                                }}
+                            >
+                                {minutes} : {seconds}
                             </Animated.Text>
                         );
                     }}
@@ -36,9 +48,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        paddingBottom: ExpoConstants.statusBarHeight,
+        paddingBottom: ExpoConstants.statusBarHeight + 20,
         backgroundColor: "#ecf0f1",
-        padding: 8,
+        // padding: 8,
+    },
+    interiorText: {
+        fontSize: 24,
     },
 });
 
